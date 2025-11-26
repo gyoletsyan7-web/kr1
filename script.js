2fe68f0a8e7c7eb5a9ea39e9b78eea3c22882cc2
gyoletsyan7-web <gyoletsyan7@gmail.com>
2025-11-26 15:44:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const flowers = [
     { name: "Розы", price: "350 руб./1 шт.", img: "./images/розы.jpeg" },
     { name: "Пионовидные розы", price: "650 руб./1 шт.", img: "./images/пионовидные розы.jpg" },
-    { name: "Пионы", price: "850 руб./1 шт.", img: "/Users/macbook/Downloads/пионы.JPG" },
+    { name: "Пионы", price: "850 руб./1 шт.", img: "./images/пионы.JPG" },
     { name: "Хризантемы", price: "450 руб./1 шт.", img: "/Users/macbook/Downloads/хризантемы.jpg" },
     { name: "Тюльпаны", price: "550 руб./1 шт.", img:"/Users/macbook/Downloads/тюльпан.jpg.webp" },
     { name: "Готовые букеты", price: "5000 руб./1 шт.", img: "/Users/macbook/Downloads/готовый букет.jpg" }
